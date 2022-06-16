dd229946b8a34166a50e91d85b8e92ee88bf43c7
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-06-16 14:29:56 -0700

    Made script work with API

--- script.js
+++ script.js
@@ -1,153 +1,88 @@
 const imageBaseUrl = 'https://image.tmdb.org/t/p'
- 
-// Example image tag
-//<img class="movie-poster" src="${imageBaseUrl}/w342${movie.posterPath}" alt="${movie.title}" title="${movie.title}"/>
-const movies = [
-   {
-   id: 338953,
-   posterPath: "/8ZbybiGYe8XM4WGmGlhF0ec5R7u.jpg",
-   title: "Fantastic Beasts: The Secrets of Dumbledore",
-   voteAverage: 6.9
-   },
-   {
-   id: 526896,
-   posterPath: "/6JjfSchsU6daXk2AKX8EEBjO3Fm.jpg",
-   title: "Morbius",
-   voteAverage: 6.4
-   },
-   {
-   id: 752623,
-   posterPath: "/neMZH82Stu91d3iqvLdNQfqPPyl.jpg",
-   title: "The Lost City",
-   voteAverage: 6.8
-   },
-   {
-   id: 675353,
-   posterPath: "/6DrHO1jr3qVrViUO6s6kFiAGM7.jpg",
-   title: "Sonic the Hedgehog 2",
-   voteAverage: 7.7
-   },
-   {
-   id: 639933,
-   posterPath: "/zhLKlUaF1SEpO58ppHIAyENkwgw.jpg",
-   title: "The Northman",
-   voteAverage: 7.3
-   },
-   {
-   id: 818397,
-   posterPath: "/QaNLpq3Wuu2yp5ESsXYcQCOpUk.jpg",
-   title: "Memory",
-   voteAverage: 7.3
-   },
-   {
-   id: 507086,
-   posterPath: "/kAVRgw7GgK1CfYEJq8ME6EvRIgU.jpg",
-   title: "Jurassic World Dominion",
-   voteAverage: 6.7
-   },
-   {
-   id: 453395,
-   posterPath: "/9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg",
-   title: "Doctor Strange in the Multiverse of Madness",
-   voteAverage: 7.4
-   },
-   {
-   id: 831946,
-   posterPath: "/cpWUtkcgRKeauhTyVMjYHxAutp4.jpg",
-   title: "Interceptor",
-   voteAverage: 6.3
-   },
-   {
-   id: 610150,
-   posterPath: "/rugyJdeoJm7cSJL1q4jBpTNbxyU.jpg",
-   title: "Dragon Ball Super: Super Hero",
-   voteAverage: 6.8
-   },
-   {
-   id: 414906,
-   posterPath: "/74xTEgt7R36Fpooo50r9T25onhq.jpg",
-   title: "The Batman",
-   voteAverage: 7.8
-   },
-   {
-   id: 628900,
-   posterPath: "/rJPGPZ5soaG27MK90oKpioSiJE2.jpg",
-   title: "The Contractor",
-   voteAverage: 6.6
-   },
-   {
-   id: 629542,
-   posterPath: "/7qop80YfuO0BwJa1uXk1DXUUEwv.jpg",
-   title: "The Bad Guys",
-   voteAverage: 7.8
-   },
-   {
-   id: 825808,
-   posterPath: "/g2n1lFIFXC0lpG32ysUhFi0Uz61.jpg",
-   title: "See for Me",
-   voteAverage: 6
-   },
-   {
-   id: 763285,
-   posterPath: "/zT5ynZ0UR6HFfWQSRf2uKtqCyWD.jpg",
-   title: "Ambulance",
-   voteAverage: 7
-   },
-   {
-   id: 648579,
-   posterPath: "/bmxCAO0tz79xn40swJAEIJPRnC1.jpg",
-   title: "The Unbearable Weight of Massive Talent",
-   voteAverage: 7.3
-   },
-   {
-   id: 361743,
-   posterPath: "/wxP2Mzv9CdjOK6t4dNnFGqIQl0V.jpg",
-   title: "Top Gun: Maverick",
-   voteAverage: 8.3
-   }
-];
+const apiKey = "7801510f3800dd1b6ec7e4d9f68f06c9";
+const basePopularRequest = "https://api.themoviedb.org/3/movie/popular?api_key=";
+const basePopularEnd = "&language=en-US&page=";
 
+const searchFirst = "https://api.themoviedb.org/3/search/movie?api_key=";
+const searchSecond = "&language=en-US&query="
+const searchThird = "&page="
+const searchFourth =  "&include_adult=false"
 
+var searchMode = false;
+
+var page = 1;
+var searchStr;
 
 const movieGridElement = document.querySelector(".movies-grid");
 const moviesLoaded = document.querySelector("#movies-loaded");
 const loadMoreButton = document.querySelector("#load-more-movies-btn");
 const searchBar = document.querySelector("#search-input");
+var numMovies = 0;
 
-console.log('moviesLoaded: ', moviesLoaded);
-let numMovies = 0;
-const moviesLoadedPerClick = 5;
-
-function loadMovie(movieGridElement, movie) {
-    movieGridElement.innerHTML +=
-    `<div class="movie-card">
-        <img class="movie-poster" src="${imageBaseUrl}/w342${movie.posterPath}" alt="${movie.title}" title="${movie.title}"/>
-        <div class="movie-title">${movie.title}</div>
-        <div class="movie-votes">Votes: ${movie.voteAverage}</div>
-    </div>`
-    console.log('movieGridElement: ', movieGridElement);
-}
-
-
-function loadMoreMovies(movieGridElement, movies, numIncrease) {
-    for(let i = 0; i < numIncrease && numMovies < movies.length; i++) {
-        loadMovie(movieGridElement, movies[numMovies]);
+async function loadMovies(movieGridElement, results) {
+    console.log('results: ', results);
+    var data = results["results"];
+    console.log('data: ', data);
+    for(var i = 0; i < data.length; i++) {
+        var movie = data[i];
+        movieGridElement.innerHTML +=
+        `<div class="movie-card">
+            <img class="movie-poster" src="${imageBaseUrl}/w342${movie.poster_path}" alt="${movie.title}" title="${movie.title}"/>
+            <div class="movie-title">${movie.title}</div>
+            <div class="movie-votes">Votes: ${movie.vote_average}</div>
+        </div>`;
         numMovies++;
     }
     moviesLoaded.innerHTML = `<h3>${numMovies} Movies Loaded</h3>`;
 }
 
-function filterMovies(str) {
-    if(str == 13) {
-        console.log("implement later");
+async function fetchMovies(movieGridElement) {
+    try {
+        const response = await fetch(basePopularRequest + apiKey + basePopularEnd + page);
+        console.log('basePopularRequest + apiKey + basePopularEnd + page: ', basePopularRequest + apiKey + basePopularEnd + page);
+        const results = await response.json();
+        console.log('results: ', results);
+        page++;
+        loadMovies(movieGridElement, results);
+    }
+    catch (error) {
+        movieGridElement.innerHTML += `<div class="error">${error}</div>`;
     }
 }
 
-function addEventListeners(loadMoreButton, searchBar) {
-    loadMoreButton.addEventListener("click", function(event) {loadMoreMovies(movieGridElement, movies, moviesLoadedPerClick); }, false);
+function resetValues() {
+    numMovies = 0;
+    page = 1;
+    movieGridElement.innerHTML = "";
+}
+
+async function filterMovies(movieGridElement, str) {
+    console.log(str);
+    searchMode = true;
+    try {
+        const response = await fetch(searchFirst + apiKey + searchSecond + str + searchThird + page + searchFourth);
+        console.log('searchFirst + apiKey + searchSecond + str + searchThird + page + searchFourth: ', searchFirst + apiKey + searchSecond + str + searchThird + page + searchFourth);
+        const results = await response.json();
+        page++;
+        loadMovies(movieGridElement, results);
+    }
+    catch (error) {
+        movieGridElement.innerHTML += `<div class="error">${error}</div>`;
+    }
+}
+
+function addEventListeners(loadMoreButton, searchBar, movieGridElement) {
+    loadMoreButton.addEventListener("click", function () {
+        if(!searchMode) fetchMovies(movieGridElement); 
+        else  {
+            filterMovies(movieGridElement, searchStr);
+        }
+    }, false);
     searchBar.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
-        alert('Enter is pressed!');
+        searchStr = document.querySelector("#search-input").value;
+        resetValues();
+        filterMovies(movieGridElement, searchStr);
     }
 });
 }
@@ -156,6 +91,7 @@ function addEventListeners(loadMoreButton, searchBar) {
 
 window.onload = function () {
     // execute your functions here to make sure they run as soon as the page loads
-    loadMoreMovies(movieGridElement, movies, moviesLoadedPerClick);
-    addEventListeners(loadMoreButton, searchBar);
+    //loadMoreMovies(movieGridElement, movies, moviesLoadedPerClick);
+    addEventListeners(loadMoreButton, searchBar, movieGridElement);
+    fetchMovies(movieGridElement);
   }
